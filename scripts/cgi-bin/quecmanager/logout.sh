eb43c67ed0b0f90c58b58b0150051e531f020d2c
Christopher Landwehr <clndwhr@gmail.com>
2025-07-25 10:39:18 -0400

    carries the clearing of the token file when logout is executed

---
+++ scripts/cgi-bin/quecmanager/logout.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+rm -f /tmp/auth_success 2>/dev/null
+
+echo "Content-Type: application/json"
+echo "Cache-Control: no-cache, no-store, must-revalidate"
+echo "Pragma: no-cache"
+echo "Expires: 0"
+echo ""
+
